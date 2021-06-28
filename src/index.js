d73fef2e55a6a551379fedd1237df5b9abecd1b5
Sergey-Markov <1nativefl@gmail.com>
2021-06-28 23:44:27 +0300

    готовое ДЗ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './sass/main.scss';
 import { fetchCountries } from './js/fetchCountries.js';
 import debounce from 'lodash.debounce';
-import  template from './handlebars/template';
+import  countryTemplate from './handlebars/countryTemplate';
+import countryListTemplate from './handlebars/countryListTemplate';
+
+import {  info, success, error, defaults } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import * as PNotifyMobile from '@pnotify/mobile';
+defaults.delay = 5000;
+
+
+  
+  
 
 const refs = {
     countrySearch: document.querySelector('.countrySearch'),
@@ -20,8 +31,37 @@ function changeValue(){
 }; 
 
 function render(response){
-    
-  refs.searchingForm.insertAdjacentHTML('afterend', template(response));
+    if(response.status === 404) {
+        const myError = error({
+            text: "The country you are looking for no found, or it does not yet exist. Try to check the search.(Страну которую Вы ищите не найдено, либо она пока не существует. Попробуйте уточнить поиск)"
+          });
+        return refs.searchingForm.innerHTML = '';
+    }
+    if(response.length > 10){
+        const myInfo = info({
+            text: "The answer has more than 10 countries - Try check search(Поиск нашел более 10 стран - уточните запрос)"
+          });
+        return refs.searchingForm.innerHTML = '';
+    }
+    if(response.length === 0) {
+        return refs.searchingForm.innerHTML = '';
+    };
+    if(response.length <= 10 && response.length >= 2 ){
+        const myInfo = info({
+            text: "Here are countries - appropriate search.(Вот страны - соответствующие поиску.)"
+          });
+        refs.searchingForm.innerHTML = countryListTemplate(response);
+    } else {
+        
+
+        const mySuccess = success({
+            title: 'Success',
+            text: "The country you were looking for.(Страна которую ты искал.)",
+           
+        });   
+
+        refs.searchingForm.innerHTML = countryTemplate(response);
+    }
     
 };
 
